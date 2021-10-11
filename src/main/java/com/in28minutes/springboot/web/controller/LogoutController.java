e223f29d9a53deb2f7024bd10ed3288da08dd00f
ravisinghrajput95 <92205137+ravisinghrajput95@users.noreply.github.com>
2021-10-11 13:35:02 +0530

    Update LogoutController.java

--- src/main/java/com/in28minutes/springboot/web/controller/LogoutController.java
+++ src/main/java/com/in28minutes/springboot/web/controller/LogoutController.java
@@ -1,4 +1,4 @@
-package com.in28minutes.springboot.web.controller;
+package com.rajputmarch2020.springboot.web.controller;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
