3a34179705cf7364c96d3c289eeb995b7dc402aa
ravisinghrajput95 <92205137+ravisinghrajput95@users.noreply.github.com>
2021-10-11 13:34:13 +0530

    Update WelcomeController.java

--- src/main/java/com/in28minutes/springboot/web/controller/WelcomeController.java
+++ src/main/java/com/in28minutes/springboot/web/controller/WelcomeController.java
@@ -1,4 +1,4 @@
-package com.in28minutes.springboot.web.controller;
+package com.rajputmarch2020.springboot.web.controller;
 
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
