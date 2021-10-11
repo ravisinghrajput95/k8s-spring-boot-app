d592ecab08395c399644f8e717f539d424a28baa
ravisinghrajput95 <92205137+ravisinghrajput95@users.noreply.github.com>
2021-10-11 13:17:29 +0530

    Update SpringBootFirstWebApplication.java

--- src/main/java/com/in28minutes/springboot/web/SpringBootFirstWebApplication.java
+++ src/main/java/com/in28minutes/springboot/web/SpringBootFirstWebApplication.java
@@ -1,4 +1,4 @@
-package com.in28minutes.springboot.web;
+package com.rajputmarch2020.springboot.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
