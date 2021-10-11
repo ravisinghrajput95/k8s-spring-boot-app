1f56a058e28829cf6c8b6fc88c7f19135a11e2c6
ravisinghrajput95 <92205137+ravisinghrajput95@users.noreply.github.com>
2021-10-11 13:45:09 +0530

    Update TodoRepository.java

--- src/main/java/com/in28minutes/springboot/web/service/TodoRepository.java
+++ src/main/java/com/in28minutes/springboot/web/service/TodoRepository.java
@@ -1,10 +1,10 @@
-package com.in28minutes.springboot.web.service;
+package com.rajputmarch2020.springboot.web.service;
 
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.in28minutes.springboot.web.model.Todo;
+import com.rajputmarch2020.springboot.web.model.Todo;
 
 public interface TodoRepository extends JpaRepository<Todo, Integer>{
 	List<Todo> findByUser(String user);
